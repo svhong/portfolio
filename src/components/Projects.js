9066e04afea70ae7dcbeefc4f3ef7f3bd729e91e
Sean <writeseanhong@gmail.com>
2019-09-04 09:34:35 -0500

    push

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,21 +12,18 @@ export default class Projects extends Component {
                 ProjectImageUrl: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
                 ProjectDescription: "Describing the project here in detail lots and lots and lotsand lotsand lotsand lotsand lotsand lotsand lots of detail",
                 ProjectTech: "These types of technologies were used",
-                ProjectHover: false
             },
             {
                 ProjectTitle: "Personal Blog Editor",
                 ProjectImageUrl: "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?cs=srgb&dl=architecture-buildings-city-325185.jpg&fm=jpg",
                 ProjectDescription: "Describing the project here in detail lots and lots and lotsand lotsand lotsand lotsand lotsand lotsand lots of detail",
                 ProjectTech: [],
-                ProjectHover: false
             },
             {
                 ProjectTitle: "Personal Blog Editor",
                 ProjectImageUrl: "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?cs=srgb&dl=architecture-buildings-city-325185.jpg&fm=jpg",
                 ProjectDescription: "Describing the project here in detail lots and lots and lotsand lotsand lotsand lotsand lotsand lotsand lots of detail",
                 ProjectTech: [],
-                ProjectHover: false
             },
 
 
